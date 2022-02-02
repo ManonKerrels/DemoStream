85783dff77d01577cc557d235f2f3be071b0c052
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-02-02 09:06:12 +0100

    1er commit

---
+++ src/main/java/servlet/jspServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.PrintWriter;
+// Traduction en java du JSP
+// du texte html :
+
+class jspServlet {
+
+//    void service(request, response){
+//        PrintWriter out = ...;
+//        out.print("...html...");
+//    }
+}
+
+// la directive d'expression :
+//<%= 1+1 %>
+
+//class JspServlet{
+//
+//    void service(request, response){
+//        PrintWriter out = ...;
+//        out.print(1+1);
+//    }
+//}
+
+// directive de scriptlet
+//<%
+//        for(int i = 0; i<5; i++){
+//        out.println("Hey")
+//        }
+//        %>
+
+//class JspServlet{
+//
+//    void service(request, response){
+//        PrintWriter out = ...;
+//        for(int i = 0; i<5; i++){
+//            out.println("Hey");
+//            out.println(i);
+//        }
+//    }
+//}
+
+// directive de déclaration
+// <%! int a = 5; %>
+
+//on écrit en dehors du service, pour définir des méthodes et des variables
+
+//class JspServlet{
+//    int a = 5;
+//
+//    void service(request, response){
+//        PrintWriter out = ...;
+//    }
+//}
